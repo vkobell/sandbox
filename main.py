888fe01b269e4d6c386b1891b81908512098919f
vkobell <vkobell4me@gmail.com>
2024-07-01 19:36:06 -0500

    avg max

---
+++ main.py
@@ -0,0 +1,13 @@
+import statistics
+
+Uinput = input().split() #written first to turn input into list
+my_list = []
+
+for num in Uinput:
+     my_list.append(int(num))
+    
+#list_avg = int
+list_avg = statistics.mean(my_list)
+my_list.sort(reverse = True)
+
+print(list_avg, my_list[0])
