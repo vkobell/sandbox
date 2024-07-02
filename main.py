ef43b5fd0fa9df5f7fe8a0b260423d660bd45255
vkobell <vkobell4me@gmail.com>
2024-07-02 11:05:57 -0500

    multi version

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import statistics
 
-Uinput = input('Please enter your input(s): ').split() #written first to turn input into list
+
+#Uinput = input().split() #written first to turn input into list//version without verbosity and zero split specific char
+Uinput = input('Please enter your input(s) and seperate with comma if needed: ').split(',') #written first to turn input into list//version WITH verbosity and char specific split function
+
 my_list = []
 
 for num in Uinput:
      my_list.append(int(num))
     
-#list_avg = int
 list_avg = statistics.mean(my_list)
 my_list.sort(reverse = True)
 
