a6024e7348cb574589e1ec3746d3d572d4b49c60
vkobell <vkobell4me@gmail.com>
2024-07-31 12:42:10 -0500

    Pushing first draft

---
+++ labs8.py
@@ -0,0 +1,15 @@
+#This is for the fat burning heart rate lab
+def get_age():
+    age = int(input())
+    # TODO: Raise exception for invalid ages
+    return age
+
+# TODO: Complete fat_burning_heart_rate() function
+def fat_burning_heart_rate(age):
+
+    return heart_rate
+
+if __name__ == "__main__":
+    # TODO: Modify to call get_age() and fat_burning_heart_rate()
+    #       and handle the exception
+    age = get_age()
