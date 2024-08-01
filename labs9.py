2a07a8c0ed8d0f9b1b56070c4f3c54b65a9a1aad
vkobell <vkobell4me@gmail.com>
2024-08-01 11:26:51 -0500

    created

---
+++ labs9.py
@@ -0,0 +1,8 @@
+#Given a list of 10 names, complete the program that outputs the name specified by the list index entered by the user.
+#Use a try block to output the name and an except block to catch any IndexError.
+#Output the message from the exception object if an IndexError is caught.
+#Output the first element in the list if the invalid index is negative or the last element if the invalid index is positive.
+names = ['Ryley', 'Edan', 'Reagan', 'Henry', 'Caius', 'Jane', 'Guto', 'Sonya', 'Tyrese', 'Johnny']
+index = int(input())
+
+# Type your code here.
