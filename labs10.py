59cf42a0190020ce5cb2ab18c5fb4f1ac770ce90
vkobell <vkobell4me@gmail.com>
2024-08-02 11:31:52 -0500

    notes

--- labs10.py
+++ labs10.py
@@ -1,5 +1,12 @@
 # Define your method here
+#2,000 steps + 1 mile
+#EX: if input is 5345 the output will be 2.67
+#EX: if the input is a negative number output will print "Exception: Negative step count entered."
 
+# Define your method here
+def steps_to_miles(steps):
+    if steps < 0:
+        raise #something
 if __name__ == '__main__':
     str = ()
     #type your code here
