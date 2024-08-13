437d66ed2baa5ea9ad0336bb44beba26d7f15fe4
vkobell <vkobell4me@gmail.com>
2024-08-13 11:53:11 -0500

    notes

--- labs13.py
+++ labs13.py
@@ -23,3 +23,18 @@ if __name__ == "__main__":
     print(f'Solutions to {a:.0f}x^2 + {b:.0f}x + {c:.0f} = 0')
     print_number(solution[0], 'x1 = ')
     print_number(solution[1], 'x2 = ')
+
+
+#Implement the quadratic_formula() function. The function takes 3 arguments, a, b, and c, and computes the two results of the quadratic formula:
+
+#The quadratic_formula() function returns the tuple (x1, x2). Ex: When a = 1, b = -5, and c = 6, quadratic_formula() returns (3, 2)
+
+#Code provided in main.py reads a single input line containing values for a, b, and c, separated by spaces. Each input is converted to a float and passed to the quadratic_formula() function.
+
+#Ex: If the input is:
+#2 -3 -77
+
+#the output is:
+#Solutions to 2x^2 + -3x + -77 = 0
+#x1 = 7
+#x2 = -5.50
