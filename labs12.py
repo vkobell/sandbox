77d70bc521c791d616069bf899203b9d941d8d9c
vkobell <vkobell4me@gmail.com>
2024-08-13 10:47:19 -0500

    added final notes and cleanup

--- labs12.py
+++ labs12.py
@@ -1,16 +1,20 @@
 import random #ODO: Import the random module
 
-def number_guess(num):
-    # TODO: Get a random number between 1-100
+def number_guess(num):     #Get a random number between 1-100
+    random_number = random.randint(1, 100)
     
-    # TODO: Compare parameter num to the random number
-    
-        
-    if __name__ == "__main__": # Use the seed 900 to get the same pseudo random numbers every time
-        random.seed(900)
-        user_input = input()
-        tokens = user_input.split()
+    if num < random_number:     #Compare parameter num to the random number
+        print(f"{num} is too low. Random number was {random_number}.")
+    elif num > random_number:
+        print(f"{num} is too high. Random number was {random_number}.")
+    else:
+        print(f"{num} is correct!")
 
-    for token in tokens: # Convert the string tokens into integers
+if __name__ == "__main__":     #Use the seed 900 to get the same pseudo random numbers every time
+    random.seed(900)
+    user_input = input()
+    tokens = user_input.split()
+
+    for token in tokens:         # Convert the string tokens into integers
         num = int(token)
         number_guess(num)
