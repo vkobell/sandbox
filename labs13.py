66b6a9be7ac7d307cc3711b3f79cd6ec558af208
vkobell <vkobell4me@gmail.com>
2024-08-13 11:12:10 -0500

    created

---
+++ labs13.py
@@ -0,0 +1,25 @@
+#Quadratic Formula
+# TODO: Import math module
+
+def quadratic_formula(a, b, c):
+    # TODO: Compute the quadratic formula results in variables x1 and x2
+    return (x1, x2)
+    
+
+def print_number(number, prefix_str):
+    if float(int(number)) == number:
+        print(f'{prefix_str}{number:.0f}')
+    else:
+        print(f'{prefix_str}{number:.2f}')
+    
+
+if __name__ == "__main__":
+    input_line = input()
+    split_line = input_line.split(" ")
+    a = float(split_line[0])
+    b = float(split_line[1])
+    c = float(split_line[2])
+    solution = quadratic_formula(a, b, c)
+    print(f'Solutions to {a:.0f}x^2 + {b:.0f}x + {c:.0f} = 0')
+    print_number(solution[0], 'x1 = ')
+    print_number(solution[1], 'x2 = ')
