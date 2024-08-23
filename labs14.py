9f2c9054d34d557248103ece2c6df8437f4fcbe4
vkobell <vkobell4me@gmail.com>
2024-08-23 13:29:24 -0500

    done

--- labs14.py
+++ labs14.py
@@ -1,3 +1,11 @@
-inputfile = input()
-upperbound = input()
-lowerbound = input()
+#declare the inputs, all three and gather them
+input_file = input()
+lower_bound = input()
+upper_bound = input()
+
+with open(input_file, 'r') as file:#open with "with" to automatically close file once done
+    words = [word.strip() for word in file.readlines()]#strip automatically removes whiteline/newline/whitespace
+
+for word in words:
+    if lower_bound <= word <= upper_bound:
+        print(word)
