f3d9907d6f98ad832b28acb6350d7ad9e10fcf52
vkobell <vkobell4me@gmail.com>
2024-08-29 14:14:52 -0500

    completed

--- labs15.py
+++ labs15.py
@@ -1,29 +1,3 @@
-#The base class Pet has attributes name and age. The derived class Dog inherits attributes from the base class Pet class and includes a breed attribute. Complete the program to:
-
-#Create a generic pet, and print the pet's information using print_info().
-#Create a Dog pet, use print_info() to print the dog's information, and add a statement to print the dog's breed attribute.
-
-#Ex: If the input is:
-
-##Dobby
-##2
-#Kreacher
-#3
-#German Schnauzer
-#the output is:
-
-#Pet Information: 
-   #Name: Dobby
-   #Age: 2
-#Pet Information: 
-   #Name: Kreacher
-   #Age: 3
-   #Breed: German Schnauzer
-
-
-
-
-
 #Code I already recieved from zylabs
 class Pet:
     def __init__(self):
@@ -50,9 +24,39 @@ dog_age = int(input())
 dog_breed = input()
 
 # TODO: Create generic pet (using pet_name, pet_age) and then call print_info()
-
+my_pet.name = pet_name
+my_pet.age = pet_age
+my_pet.print_info()
 
 # TODO: Create dog pet (using dog_name, dog_age, dog_breed) and then call print_info()
 
+my_dog.name = dog_name
+my_dog.age = dog_age
+my_dog.breed = dog_breed
+my_dog.print_info()
+
 # TODO: Use my_dog.breed to output the breed of the dog
 
+print ('   Breed:', my_dog.breed)
+
+#The base class Pet has attributes name and age. The derived class Dog inherits attributes from the base class Pet class and includes a breed attribute. Complete the program to:
+
+#Create a generic pet, and print the pet's information using print_info().
+#Create a Dog pet, use print_info() to print the dog's information, and add a statement to print the dog's breed attribute.
+
+#Ex: If the input is:
+
+##Dobby
+##2
+#Kreacher
+#3
+#German Schnauzer
+#the output is:
+
+#Pet Information: 
+   #Name: Dobby
+   #Age: 2
+#Pet Information: 
+   #Name: Kreacher
+   #Age: 3
+   #Breed: German Schnauzer
